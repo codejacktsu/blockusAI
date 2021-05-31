8b1b4b60be69570512e3a0e1c82ff26d1e7618c3
codejacktsu <65304002+codejacktsu@users.noreply.github.com>
2021-05-30 20:15:52 -0700

    update gitignore + tutorial

--- quickstart.py
+++ quickstart.py
@@ -1,9 +1,12 @@
 import gym
 
 
-env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-env.close()
+env = gym.make('CartPole-v1')
+# env.reset()
+print("Observation space:", env.observation_space)
+print("Shape:", env.observation_space.shape)
+print("Action space:", env.action_space)
+# for _ in range(1000):
+#     env.render()
+#     env.step(env.action_space.sample()) # take a random action
+# env.close()
